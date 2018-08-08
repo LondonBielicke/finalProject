93805e67c5f622c5a7712d562d95a958fe855dfe
London Bielicke <londonbielicke@Londons-MacBook-Pro.local>
2018-08-08 16:03:24 -0500

    halfway through the todo list

---
+++ finalProject/HomeworkChecklist.swift
@@ -0,0 +1,14 @@
+//
+//  HomeworkChecklist.swift
+//  finalProject
+//
+//  Created by London Bielicke on 8/8/18.
+//  Copyright © 2018 London Bielicke. All rights reserved.
+//
+
+import UIKit
+
+class HomeworkChecklist {
+    var name = ""
+    var dueSoon = false 
+}
